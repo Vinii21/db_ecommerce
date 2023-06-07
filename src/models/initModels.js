b386ac7b3720b755b028b8d22192949ee019981c
Vinicio Chavarria <vichavarria21@gmaail.com>
2023-06-07 14:41:44 -0600

    Relaciones terminadas

--- src/models/initModels.js
+++ src/models/initModels.js
@@ -6,11 +6,9 @@ const ProductInCar = require("./productInCar.model")
 const Orders = require("./orders.model")
 const ProductInOrder = require("./ProductInOrder.model")
 
-
 const initModels = () => {
 
   Users.hasOne(Cars, {foreignKey:"userId"});
-  Cars.hasOne(Users, {foreignKey: "userId"});
 
   Orders.belongsTo(Users, {foreignKey:"userId"});
   Users.hasMany(Orders, {foreignKey:"userId"});
@@ -18,15 +16,15 @@ const initModels = () => {
   Products.belongsTo(Users, {foreignKey: "userId"});
   Users.hasMany(Products, {foreignKey: "userId"});
   
-  Orders.belongsToMany(Products, { through: ProductInOrder, foreignKey:"orderId", otherKey:"productId", timestamps: false});
-  Products.belongsToMany(Orders, { through: ProductInOrder, foreignKey:"productId", otherKey:"orderId", timestamps: false});
+  ProductInOrder.belongsTo(Orders, {foreignKey: "orderId"})
+  Orders.hasMany(ProductInOrder, {foreignKey: "orderId"})
+  ProductInOrder.belongsTo(Products, {foreignKey:"productId"})
+  Products.hasMany(ProductInOrder, {foreignKey: "productId"})
 
-  Cars.belongsToMany(Products, { through: ProductInCar, foreignKey:"carId", otherKey: "productId", timestamps: false});
-  Products.belongsToMany(Cars, { through: ProductInCar, foreignKey:"productId", otherKey:"carId", timestamps: false});
-
-/*   Users.belongsToMany(Products, { through: ProductInOrder, foreignKey: "userId", otherKey: "conversationId", timestamps: false });
-  Products.belongsToMany(Users, { through: ProductInCar, foreignKey: "conversationId", otherKey: "userId", timestamps: false }); */
-
+  ProductInCar.belongsTo(Cars, {foreignKey:"carId"})
+  Cars.hasMany(ProductInCar, {foreignKey:"carId"})
+  ProductInCar.belongsTo(Products, {foreignKey:"productId"})
+  Products.hasMany(ProductInCar, {foreignKey:"productId"})
 };
 
 module.exports = initModels;
