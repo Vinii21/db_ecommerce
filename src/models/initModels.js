d40492be52435a20505faa555c65e575491e4209
Vinicio Chavarria <vichavarria21@gmaail.com>
2023-06-07 17:26:28 -0600

    Comillas en el init model

--- src/models/initModels.js
+++ src/models/initModels.js
@@ -16,15 +16,15 @@ const initModels = () => {
   Products.belongsTo(Users, {foreignKey: "userId"});
   Users.hasMany(Products, {foreignKey: "userId"});
   
-  ProductInOrder.belongsTo(Orders, {foreignKey: "orderId"})
-  Orders.hasMany(ProductInOrder, {foreignKey: "orderId"})
-  ProductInOrder.belongsTo(Products, {foreignKey:"productId"})
-  Products.hasMany(ProductInOrder, {foreignKey: "productId"})
+  ProductInOrder.belongsTo(Orders, {foreignKey: "orderId"});
+  Orders.hasMany(ProductInOrder, {foreignKey: "orderId"});
+  ProductInOrder.belongsTo(Products, {foreignKey:"productId"});
+  Products.hasMany(ProductInOrder, {foreignKey: "productId"});
 
-  ProductInCar.belongsTo(Cars, {foreignKey:"carId"})
-  Cars.hasMany(ProductInCar, {foreignKey:"carId"})
-  ProductInCar.belongsTo(Products, {foreignKey:"productId"})
-  Products.hasMany(ProductInCar, {foreignKey:"productId"})
+  ProductInCar.belongsTo(Cars, {foreignKey:"carId"});
+  Cars.hasMany(ProductInCar, {foreignKey:"carId"});
+  ProductInCar.belongsTo(Products, {foreignKey:"productId"});
+  Products.hasMany(ProductInCar, {foreignKey:"productId"});
 };
 
 module.exports = initModels;
