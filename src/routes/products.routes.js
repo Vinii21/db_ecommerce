2c0153fb5d3d17fd5b9eaddc9efa3c5389248911
Vinicio Chavarria <vichavarria21@gmaail.com>
2023-06-11 13:34:23 -0600

    ..

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,18 +1,17 @@
 const { Router } = require("express");
 const { createProductController, updateDescriptionController, getProductUserMayorController } = require("../controllers/products.controlles");
 const upload = require("../middlewares/multer.middleware");
-const { createProductValidator, updateDescriptionValidator } = require("../validators/products.validators");
-
+const { updateDescriptionValidator } = require("../validators/products.validators");
 const authenticate = require("../middlewares/auth.middleware");
-const { createProductValidator } = require("../validators/products.validators");
+
 
 const router = Router();
 
-router.post("/products", authenticate, createProductValidator, upload, createProductController);
+router.post("/products", authenticate, upload, createProductController);
 router.put("/products", authenticate, updateDescriptionController);
 router.post("/products", upload, createProductController);
 router.put("/products", updateDescriptionValidator, updateDescriptionController);
-router.get("/products", getProductUserMayorController);
+router.get("/products", authenticate, getProductUserMayorController);
 
 module.exports = router;
 
